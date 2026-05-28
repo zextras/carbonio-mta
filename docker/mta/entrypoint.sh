961612c2f8edacbcc27ae9baab3a4322084f5ca6
Gianluca Boiano <morf3089@gmail.com>
2026-05-28 13:08:59 +0200

    fix(entrypoint): patch empty sample_directory in arm64 carbonio-postfix
    The arm64 build of carbonio-postfix ships main.cf with an empty
    `sample_directory =` line. Any postconf/postfix invocation then aborts
    with:
    
      postfix: fatal: bad string length 0 < 1: sample_directory =
    
    amd64 ships the same file with sample_directory = /opt/zextras/common/conf
    populated, so this is an arm64 packaging bug. Until the deb is fixed,
    patch the value at container start so the image works on both arches.

--- docker/mta/entrypoint.sh
+++ docker/mta/entrypoint.sh
@@ -10,6 +10,12 @@ sed -i -e "s/LDAP_PORT/${LDAP_PORT}/g" /opt/zextras/conf/*.cf
 sed -i -e "s/LDAP_ROOT_PASSWORD/${LDAP_ROOT_PASSWORD}/g" /opt/zextras/conf/*.cf
 sed -i -e "s#LDAP_URL#${LDAP_URL}#g" /opt/zextras/conf/*.cf
 
+# Workaround: arm64 carbonio-postfix ships main.cf with an empty
+# `sample_directory =` which postfix rejects ("bad string length 0 < 1").
+# Set a valid value before any postconf/postfix invocation.
+sed -i 's|^sample_directory = *$|sample_directory = /opt/zextras/common/conf|' \
+    /opt/zextras/common/conf/main.cf
+
 /opt/zextras/common/sbin/postconf maillog_file=/var/log/postfix.log
 
 /opt/zextras/common/sbin/postfix start
